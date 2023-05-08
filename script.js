8484c7d8370120ef906ea005e0f7233df2b3db54
krishnagudladana <krishnagc108@gmail.com>
2023-05-08 22:02:35 +0530

    changes committed

--- script.js
+++ script.js
@@ -24,9 +24,24 @@ document.querySelector('.guess').value = 23;
 
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    
+if(!guess) {
+    document.querySelector('.message').textContent = 'Please guess a number';
+}else if(guess === secretNumber){
+    document.querySelector('.message').textContent = ' Correct Number';
+}else if(guess > secretNumber) {
+    document.querySelector('.message').textContent = ' Too High';
+}else if(guess < secretNumber) {
+    document.querySelector('.message').textContent = ' Too Low';
+}
 });
 
 //In the above line, we are selecting a button and we are listening for any click on the button by using the addEventListener method. As we are listening for a click, we pass it as argument in addEventListener method.
