6bd45985d2a3a5d5fba3a9eebd00af2b98c15341
krishnagudladana <krishnagc108@gmail.com>
2023-05-08 23:24:36 +0530

    changes committed

--- script.js
+++ script.js
@@ -24,28 +24,85 @@ document.querySelector('.guess').value = 23;
 
 console.log(document.querySelector('.guess').value);
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
+// document.querySelector('.number').textContent = secretNumber;
 
 
 document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
+    let guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
-    
-if(!guess) {
-    document.querySelector('.message').textContent = 'Please guess a number';
-}else if(guess === secretNumber){
-    document.querySelector('.message').textContent = ' Correct Number';
-}else if(guess > secretNumber) {
-    document.querySelector('.message').textContent = ' Too High';
-}else if(guess < secretNumber) {
-    document.querySelector('.message').textContent = ' Too Low';
-}
+    //In the above line, we are selecting a button and we are listening for any click on the button by using the addEventListener method. As we are listening for a click, we pass it as argument in addEventListener method.
+
+    // As soon as the event listener method get a click response, it has to react the click. So we are writing a function in the even listener method. This is called as event handler. 
+
+    //In the above code, we selected the check button using querySelector and we used addEventListener method on the element to attach event handler which is the function which is used to retrieve data from the guess element using the value method. We passed to arguments in the event  handler function. First one is a click and second one is the function to retrieve the value from the guess element.
+
+    // When there is not input
+    if(!guess) {
+        displayMessage('Please guess a number');
+    }//When the player guess the correct number
+    else if(guess === secretNumber){
+        displayMessage(' Correct Number');//This shows the text as correct number if guess is equal to secret number 
+        document.querySelector('body').style.backgroundColor = '#60b347';//this changes the background of the page if guess is equal to secret number
+        document.querySelector('.number').style.width = '30rem';//this increases the width of the secret number if guess is equal to secret number
+        document.querySelector('.number').textContent = secretNumber;//this shows the secret number if guess is equal to secret number
+        //we are updating the highscore when ever the scare is greater than highscore
+        if(score > highScore) {
+            highScore = score
+            document.querySelector('.highscore').textContent = score;
+        }
+    }//When player guess the high wrong number
+    /*else if(guess > secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = ' Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+        
+    }//wheen player guess the low wring number
+    else if(guess < secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = ' Too Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+    }*///When guess is wrong and we are using ternary operator to simplify the code
+    else if(guess !== secretNumber){
+        if(score > 1) {
+            displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
+            score--;
+            document.querySelector('.score').textContent = score;
+        }else {
+            displayMessage('You lost the game');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
 });
 
-//In the above line, we are selecting a button and we are listening for any click on the button by using the addEventListener method. As we are listening for a click, we pass it as argument in addEventListener method.
-
-// As soon as the event listener method get a click response, it has to react the click. So we are writing a function in the even listener method. This is called as event handler. 
-
-//In the above code, we selected the check button using querySelector and we used addEventListener method on the element to attach event handler which is the function which is used to retrieve data from the guess element using the value method. We passed to arguments in the event  handler function. First one is a click and second one is the function to retrieve the value from the guess element.
+//We are resetting everything by using click function on the again button
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+});
